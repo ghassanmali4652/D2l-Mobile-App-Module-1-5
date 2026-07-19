97e83bd66c1226b22c5fb0ca766f3594bb692846
luanthnhnguyen <luanthnhnguyen@gmail.com>
2026-07-18 23:06:52 -0400

    Delete CourseModule.kt

--- CourseModule.kt
+++
@@ -1,7 +0,0 @@
-package com.example.courseapp.data.model
-
-data class CourseModule(
-    val id: String,          // e.g., "L6"
-    val title: String,       // e.g., "App Navigation"
-    val resources: List<ResourceLink>
-)
